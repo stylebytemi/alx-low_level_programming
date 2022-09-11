f3b3a9379e1b6b5be065c8498fb24a990d02be90
stylebytemi <teemaybelle@gmail.com>
2022-09-10 17:19:11 -0700

    add 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char rev;
+for (rev = 'z'; rev >= 'a'; rev--)
+putchar(rev);
+putchar('\n');
+return (0);
+}
