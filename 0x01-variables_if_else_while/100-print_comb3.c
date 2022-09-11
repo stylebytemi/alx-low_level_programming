e4464e08fb5e697ef5407a6caf922ebe5974376c
stylebytemi <teemaybelle@gmail.com>
2022-09-11 07:41:16 -0700

    add latest edit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,7 +11,7 @@ int main(void)
 int a, b;
 for (a = '0'; a < '9'; a++)
 {
-for (b = 0; b < '9'; b++)
+for (b = a + 1; b < '9'; b++)
 {
 if (a != b)
 {
@@ -19,7 +19,6 @@ putchar(a);
 putchar(b);
 if (a != '8' && b != '9')
 {
-continue;
 putchar(',');
 putchar(' ');
 }
