c4d80f46c4cac224defc3ad6211d1cd6e625c75a
stylebytemi <teemaybelle@gmail.com>
2022-09-13 19:59:40 -0700

    add

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,28 +1,17 @@
 #include <stdio.h>
 /**
-* main-computes and print the sum of all the multiples
-* of 3 or 5 below 1024
-* Return: 0 if sucessful
+* main - Entry point
+*
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-unsigned long int sum3, sum5, sum;
-int i;
-sum3 = 0;
-sum5 = 0;
-sum = 0;
-for (i = 0; i < 1024; ++i)
+int i, sum = 0;
+for (i = 0; i < 1024; i++)
 {
-if ((i % 3) == 0)
-{
-sum3 = sum3 + i;
+if ((i % 5) == 0 || (i % 3) == 0)
+sum += i;
 }
-else if ((i % 5) == 0
-{
-sum5 = sum5 + i;
-}
-}
-sum = sum3 + sum5;
-printf("%lu\n", sum);
+printf("%d\n", sum);
 return (0);
 }
