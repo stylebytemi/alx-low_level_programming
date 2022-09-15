64b670d9ea7580891fb18792df667c65bcd908c7
stylebytemi <teemaybelle@gmail.com>
2022-09-15 13:14:47 -0700

    add 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _isupper-evaluate if letter is uppercase
+*@p: print int
+* Return: 0 if successful
+*/
+int _isupper(int p)
+{
+if (p >= 'A' && p <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+solutions to ALX 0x04-more_functions_nested_loops tasks
