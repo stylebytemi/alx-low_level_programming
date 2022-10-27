830fbc9737c8760ccaaea32be8c7c36b6a0621e1
stylebytemi <teemaybelle@gmail.com>
2022-10-27 13:54:53 -0700

    add 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,4 @@
-#include "mainn.h"
+#include "main.h"
 /**
 * print_binary - A function that prints a binary number without % or /
 * @n: The number to be printed
@@ -18,7 +18,7 @@ if (flag == 1 && (n & max) == 0)
 _putchar('0');
 else if ((n & max) != 0)
 {
-putchar('1');
+_putchar('1');
 flag = 1;
 }
 max >>= 1;
