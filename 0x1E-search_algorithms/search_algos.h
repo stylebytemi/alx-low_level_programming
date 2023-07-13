ec72e3e56b78cbb54c94b927d9d2dfb08431ace1
Temiloluwa Adewale <98469754+stylebytemi@users.noreply.github.com>
2023-07-13 02:55:12 +0100

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,25 +1,35 @@
-#include "search_algos.h"
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
-/**
- * linear_search - searches for a value in a sorted array of integers
- * @array: array of integers
- * @size: size of array
- * @value: value to search for
- * Return: index of value or -1 if not found
- */
+/* helpers */
+void print_array(int *array, size_t inc, size_t size);
 
-
-int linear_search(int *array, size_t size, int value)
+typedef struct skiplist_s
 {
-	size_t inc;
+				int n;
+				size_t index;
+				struct skiplist_s *next;
+				struct skiplist_s *express;
+} skiplist_t;
 
-	if (array == NULL)
-		return (-1);
-	for (inc = 0; inc < size; inc++)
-	{
-		printf("Value checked array[%ld] = [%d]\n", inc, array[inc]);
-		if (array[inc] == value)
-			return (inc);
-	}
-	return (-1);
-}
+typedef struct listint_s
+{
+		int n;
+		size_t index;
+		struct listint_s *next;
+} listint_t;
+
+/* main functions */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+#endif
