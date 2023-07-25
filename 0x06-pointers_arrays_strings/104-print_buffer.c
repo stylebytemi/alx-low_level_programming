9c270c5abaee7a36b251be94e7584545169b8612
Temiloluwa Adewale <98469754+stylebytemi@users.noreply.github.com>
2023-07-25 02:00:29 +0100

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,41 +1,48 @@
-#include "main.h"
 #include <stdio.h>
+
 /**
-*print_buffer-Print a buffer 10 bytes per line.
-*@b: Buffer address.
-*@size: Number of characters to be printed.
-*Return: no return.
-*/
+ * print_buffer - prints buffer of a char string
+ * @b: buffer to print
+ * @size: size of buffer
+ * Return: nothing
+ */
+
 void print_buffer(char *b, int size)
 {
-int j, k, l;
-if (size <= 0)
-printf("\n");
-else
-{
-for (j = 0; j < size; j += 10)
-{
-printf("%.8x:", j);
-for (k = j; k < j + 10; k++)
-{
-if (k % 2 == 0)
-printf("  ");
-if (k < size)
-printf("%.2x", *(b + k));
-else
-printf("  ");
-}
-printf(" ");
-for (l = j; l < j + 10; l++)
-{
-if (l >= size)
-break;
-if (*(b + l) < 32 || *(b + l) > 126)
-printf("%c", '.');
-else
-printf("%c", *(b + l));
-}
-printf("\n");
-}
-}
+	int i, j;
+
+	if (size <= 0)
+		printf("\n");
+	else
+	{
+		for (i = 0; i < size; i += 10)
+		{
+			printf("%08x: ", i);
+			j = 0;
+			while (j < 10)
+			{
+				if (j % 2 == 0 && j != 0)
+					printf(" ");
+				if ((j + i) > size - 1)
+				{
+					printf("  ");
+				}
+				else
+					printf("%.2x", b[i + j]);
+				j++;
+			}
+			printf(" ");
+			j = 0;
+			while (j < 10)
+			{
+				if ((j + i) > size - 1)
+					break;
+				if (b[j + i] <= 31 || b[j + 1] >= '~')
+					b[j + i] = '.';
+				putchar(b[j + i]);
+				j++;
+			}
+			printf("\n");
+		}
+	}
 }
